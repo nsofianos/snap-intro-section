c3996e41d8990a375a603ec4bad70ec58a2de50e
nsofianos <nsofianos@hotmail.com>
2022-07-09 13:41:35 -0700

    add overlay when sidebar open

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -2,8 +2,11 @@ window.onload = () => {
   const menuToggle = document.querySelector(".menu-toggle");
   const sidebar = document.querySelector(".sidebar");
   const hamburger = document.querySelector(".hamburger");
+  const overlay = document.querySelector(".overlay");
+
   menuToggle.addEventListener("click", () => {
-    sidebar.classList.toggle("is-active");
+    overlay.classList.toggle("active");
+    sidebar.classList.toggle("active");
     sidebar.classList[1]
       ? (hamburger.src = "./images/icon-close-menu.svg")
       : (hamburger.src = "./images/icon-menu.svg");
