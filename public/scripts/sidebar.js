fe4d71c00703191980ddcdfb79a6464cec48abc0
nsofianos <nsofianos@hotmail.com>
2022-07-09 16:14:27 -0700

    add company submenu, reverse arrow icons when open/close

--- public/scripts/sidebar.js
+++ public/scripts/sidebar.js
@@ -3,20 +3,27 @@ window.onload = () => {
   const sidebar = document.querySelector(".sidebar");
   const hamburger = document.querySelector(".hamburger");
   const overlay = document.querySelector(".overlay");
-  const featuresToggle = document.querySelector(".features-toggle");
-  const features = document.querySelector(".features");
+  const submenuToggle = document.querySelectorAll(".submenu-toggle");
   const subMenu = document.querySelectorAll(".sub-menu");
 
   menuToggle.addEventListener("click", () => {
     overlay.classList.toggle("active");
     sidebar.classList.toggle("active");
-    sidebar.classList[1]
-      ? (hamburger.src = "./images/icon-close-menu.svg")
-      : (hamburger.src = "./images/icon-menu.svg");
+    hamburger.src =
+      (sidebar.classList[1] && "./images/icon-close-menu.svg") ||
+      "./images/icon-menu.svg";
     subMenu.forEach((menu) => menu.classList.remove("active"));
   });
 
-  featuresToggle.addEventListener("click", () => {
-    features.classList.toggle("active");
+  submenuToggle.forEach((toggle) => {
+    toggle.addEventListener("click", () => {
+      //reverse arrow icon, toggle active class
+      const arrowIcon = toggle.childNodes[1];
+      arrowIcon.src =
+        (toggle.nextElementSibling.classList.contains("active") &&
+          "./images/icon-arrow-down.svg") ||
+        "./images/icon-arrow-up.svg";
+      toggle.nextElementSibling.classList.toggle("active");
+    });
   });
 };
